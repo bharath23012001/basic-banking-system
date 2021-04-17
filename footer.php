419e60d7c7c898d814641f1996cdabf282f37ba3
bharath23012001 <81075554+bharath23012001@users.noreply.github.com>
2021-04-17 16:18:59 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+	<title>Untitled Document</title>
+	<link rel = "stylesheet" type = "text/css" href = "mystyle.css">
+	<link rel = "stylesheet" type = "text/css" href = "menu.css">
+</head>
+<body>
+	<table width="1340" bgcolor="#999898" style=" color:white; bottom:0px; font-size:20px;">
+		<tr>
+		<td class = "f" align="center">The Sparks Foundation internship project</td>
+		<td class = "f" align="center">Done by Bharath</td>
+		</tr>
+	</table>
+</body>
+</html>
